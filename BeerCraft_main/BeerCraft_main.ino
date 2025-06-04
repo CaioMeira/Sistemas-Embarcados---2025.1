459b673cdae8b7640fe497a9daaac994566607bd
CaioMeira <63150605+CaioMeira@users.noreply.github.com>
2025-06-04 19:28:13 -0300

    Add files via upload

---
+++ BeerCraft_main/BeerCraft_main.ino
@@ -0,0 +1,16 @@
+  // ===== MAIN PROJETO BEERCRAFT ======= //
+
+  #include Statechart.h
+  #include sc_timer_service.h
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
